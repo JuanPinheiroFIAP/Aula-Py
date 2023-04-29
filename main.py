0ea86ac186b0acf2162a905ddbc31d0c01d95bc3
logonrmlocal <rm552202@fiap.com.br>
2023-04-28 22:47:28 -0300

    Primeiro_commit

---
+++ main.py
@@ -0,0 +1,66 @@
+import math
+# opcao = input("Escolha uma cidade: ")
+#
+# match opcao:
+# case "São Paulo":
+# print("Plataforma 1")
+# case "Rio de Janeiro":
+# print("Plataforma 2 ")
+# case "Curitiba":
+# print("Plataforma 3 ")
+# case "Brasilia":
+# print("Plataforma 4 ")
+# case "Florianópolis":
+# print("Plataforma 4 ")
+# case _:
+# print("voçê escolhu a opção 1 ")
+#
+# print('Casa Valduga Gran codigo: 1')
+# print("Casa Silva Gran Terrois los Carménère codigo: 2")
+# print("Chocalan Origen Viognier codigo: 3")
+# print("Miolo Demi-sec Reserva especial 2005 codigo: 4" )
+# print("Casa Silva Terrois Cabernet Savignon codigo: 5")
+# # menu = int(input("Escolha uma das opções: "))
+#
+# match menu:
+# case 1:
+# print('Casa Valduga Gran R$ 160,00')
+# case 2:
+# print('Casa Silva Gran Terrois los Carménère R$ 130,00')
+# case 3:
+# print('Chocalan Origen Viognier R$ 180,00')
+# case 4:
+# print('Miolo Demi-sec Reserva especial 2005 R$ 90,00')
+# case 5:
+# print('Casa Silva Terrois Cabernet Savignon R$ 100,00')
+
+# print('Vamos testar nossa calculadora')
+# n1 = float(input('Me informe valor do seu primeiro número: '))
+# n2 = float(input('Me informe valor do seu segundo número: '))
+# print('''
+# [1] Adicção
+# [2] multiplicação
+# [3] Subtração
+# [4] Divisão''')
+#
+# escolha = int(input("Qual operação você quer realizar? "))
+#
+# match escolha:
+# case 1:
+# s = n1 + n2
+# print(f'a soma de {n1} + {n2} é {s}')
+# case 2:
+# s = n1 * n2
+# print(f'a multiplicação de {n1} * {n2} é {s}')
+# case 3:
+# s = n1 - n2
+# print(f'a subtração de {n1} - {n2} é {s}')
+# case 4:
+# if n2 != 0:
+# s = n1 / n2
+# print(f'a divisão de {n1} / {n2} é {s}')
+# else:
+# print('Não é possivel dividir por zero')
+# case _:
+# print('Opão invalida')
+
